8c70662f6fc8ee69116e01809216008dc46f750d
kekewu0013 <84558445+kekewu0013@users.noreply.github.com>
2021-11-02 12:47:39 +0800

    Update structures.h

--- structures.h
+++ structures.h
@@ -6,20 +6,19 @@
 #define LEFT 3
 #define RIGHT 4
 
-#define OVERALL_WINDOW_WIDTH 640
-#define OVERALL_WINDOW_HEIGHT 480
+#define OVERALL_WINDOW_WIDTH 650
+#define OVERALL_WINDOW_HEIGHT 740
 
 #define ROBOT_WIDTH 20
 #define ROBOT_HEIGHT 20
 
 #define WALL_WIDTH 10
 
-#define DEFAULT_ANGLE_CHANGE 15             //it's important cause it changed the angle of turning left or right
+#define DEFAULT_ANGLE_CHANGE 15
 #define DEFAULT_SPEED_CHANGE 1
 #define MAX_ROBOT_SPEED 25
 #define PI 3.14159265
-#define SENSOR_VISION 30
-// #define SENSOR_VISION 45
+#define SENSOR_VISION 45
 
 struct Wall {
     int x,y;
